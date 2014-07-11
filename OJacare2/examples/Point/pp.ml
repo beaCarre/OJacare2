5e232c100604b58d5fb3631258a5293eb2fcdbae
beaCarre <beatrice.carre@gmail.com>
2014-07-11 17:22:59 +0200

    examples

--- OJacare2/examples/Point/pp.ml
+++
@@ -1,7 +0,0 @@
-object (self)
-  inherit _souche_jColoredPoint (init_stub_colored_point _p0 _p1 _p2)
-end;;
-
-object (self)
-  inherit _souche_jColoredPoint init_stub_colored_point _p0 _p1 _p2
-end;;
